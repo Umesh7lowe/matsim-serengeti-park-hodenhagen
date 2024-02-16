151d317ff1c6ca6f42fcb707af938c873e4200bd
umesh <umeshlowe@gmail.com>
2024-02-16 16:42:36 +0000

    event handler to get the travel time

--- src/main/java/org/matsim/analysis/SimplepersonEventHandler.java
+++ src/main/java/org/matsim/analysis/SimplepersonEventHandler.java
@@ -1,21 +1,32 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.population.Person;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimplepersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+
+    private final Map<Id<Person>, Double> personToDepartureTime = new HashMap<>();
     @Override
-    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
-        System.out.println("Arrival:" +personArrivalEvent.getTime()+":"+ personArrivalEvent.getPersonId());
+    public void handleEvent(PersonDepartureEvent personDepartureEvent) {
+        var departureTime = personDepartureEvent.getTime();
+        var personId = personDepartureEvent.getPersonId();
+        personToDepartureTime.put(personId,departureTime);
     }
 
     @Override
-    public void handleEvent(PersonDepartureEvent personDepartureEvent) {
+    public void handleEvent(PersonArrivalEvent personArrivalEvent) {
+        var arrivalTime = personArrivalEvent.getTime();
+        var departureTime = personToDepartureTime.get(personArrivalEvent.getPersonId());
+        var travelTime = arrivalTime - departureTime;
 
-        System.out.println("Departure:" +personDepartureEvent.getTime()+":"+ personDepartureEvent.getPersonId());
-
+        System.out.println("Person " + personArrivalEvent.getPersonId()+ " travelled "+ travelTime + " s");
     }
 }
 
